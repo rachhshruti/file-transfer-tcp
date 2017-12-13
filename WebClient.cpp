4876cb5a02b4fd7c6a2edd3c056ea61c75a26b98
SR053075 <shruti.rachh@cerner.com>
2017-12-13 10:24:41 -0600

    implemented file transfer from server to client using tcp

---
+++ WebClient.cpp
@@ -0,0 +1,321 @@
+#include<iostream>
+#include<cstdlib>
+#include <unistd.h>
+#include<string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include<fstream>
+#include <sstream>
+#include <arpa/inet.h>
+#include <sys/time.h> 
+using namespace std;
+
+/**
+ * The WebClient class is used to send requests for files to the server. 
+ * The client can request for either persistent
+ * or non-persistent connections. 
+ * @author Shruti Rachh
+ *
+ */
+class WebClient
+{
+	private:
+	int clientSockFileDesc;
+	struct sockaddr_in serverAddress;
+	char buff[1009630];
+	struct hostent *server;
+
+	public: 
+	void displayError(const char *errorMsg);
+	void createSocket();
+	void getServerInfo(char* hostname);
+	void setServerAddress(int portNo);
+	void connectToServer();
+	string* setRequestHeaders(string connectionType);
+	string createRequest(string filename,string connectionType);
+	int sendRequest(string request);
+	int readResponse();
+	void displayResponse(string response);
+	void closeConnection();
+};
+
+/**
+ * This method is used to display the error message.
+ * @param
+ * 	errorMsg: The error message that is to be displayed.
+*/
+void WebClient::displayError(const char *errorMsg)
+{
+	cerr<<"Error: "<<errorMsg<<endl;
+	exit(1);
+}
+
+/**
+ * This method is used to create the client socket and sets the 
+ * clientSockFileDesc class variable.
+ */
+void WebClient::createSocket()
+{
+	clientSockFileDesc=socket(AF_INET,SOCK_STREAM,0);
+	if (clientSockFileDesc < 0)
+  	{
+		displayError("The server socket could not be opened!");
+	}
+}
+
+/**
+ * This method is used to get the IP address of the server
+ * based on the hostname provided by the client.
+ * @param
+ *	hostname: the server hostname
+ */
+void WebClient::getServerInfo(char* hostname)
+{
+	server=gethostbyname(hostname);
+	if(server==NULL)
+	{
+		displayError("There is no such host!");
+	}
+}
+
+/**
+ * This method is used to set the server address.
+ * @param
+ *	portNo: the port number on which server is listening.
+ */
+void WebClient::setServerAddress(int portNo)
+{
+	bzero((char *) &serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,(char *)&serverAddress.sin_addr.s_addr,server->h_length);
+	serverAddress.sin_port = htons(portNo);
+
+}
+
+/**
+ * This method is used to connect to the server.
+ */
+void WebClient::connectToServer()
+{
+	if (connect(clientSockFileDesc,(sockaddr*)&serverAddress,sizeof(serverAddress)) < 0)
+  	{
+		displayError("Cannot connect to server!");
+	}
+}
+
+/**
+ * This method is used by the createRequest method in order to set the
+ * headers of the request. 
+ * @param
+ *	connectionType: The connection type will be either close for non-persistent connection
+ *			or keep-alive for persistent connection.
+ * @return
+ *	string array consisting of all the request headers
+ */
+string* WebClient::setRequestHeaders(string connectionType)
+{
+	string* headers=new string[5];
+	char tmpServerAddr[32];
+	const char* dottedDecimalStr=inet_ntop(AF_INET,(void *)&serverAddress.sin_addr.s_addr,tmpServerAddr,sizeof(tmpServerAddr));
+	stringstream serverPort;
+	serverPort<<ntohs(serverAddress.sin_port);	
+	headers[0]="Host: "+string(dottedDecimalStr)+":"+serverPort.str()+"\r\n";
+	headers[1]="Connection: "+connectionType+"\r\n";
+	headers[2]="Accept: text/plain; charset='UTF-8'\r\n";
+	headers[3]="Accept-Encoding: gzip, deflate, sdch\r\n";
+	headers[4]="Accept-Language: en-US,en;q=0.8\r\n";
+	return headers;
+}
+
+/**
+ * This method is used to create the request.
+ * @param
+ *	filename: the name of the file that is to be requested.
+ * @param
+ *	connectionType: The connection type will be either close for non-persistent connection
+ *			or keep-alive for persistent connection.
+ * @return
+ *	the request string to be sent
+ */
+string WebClient::createRequest(string filename,string connectionType)
+{
+	string* headers=setRequestHeaders(connectionType);
+	string requestLine="GET /"+filename+" HTTP/1.1";
+	string request=requestLine+"\r\n";
+	
+	for(int it=0;it<5;it++)
+	{
+		request=request+headers[it];
+	}
+	return request;
+}
+
+/**
+ * This method is used to send the request to the server.
+ * @param
+ *	request: the request to be sent.
+ * @return
+ * 	the number of characters written.
+ */
+int WebClient::sendRequest(string request)
+{
+	bzero(buff,1009629);
+  
+	strncpy(buff,request.c_str(),1009628);
+	int noOfCharacters=write(clientSockFileDesc,buff,strlen(buff));
+  	if (noOfCharacters<0)
+   	{
+		displayError("There is problem while writing to socket!");
+	}
+	return noOfCharacters;
+}
+
+/**
+ * This method reads the server response into a buffer.
+ * @return 
+ * 	the number of characters read.
+ */
+int WebClient::readResponse()
+{
+	bzero(buff,1009630);
+	int no=read(clientSockFileDesc,buff,32000);	
+	string resp=string(buff);
+	displayResponse(resp);	
+	string contentLengthHeader="Content-Length: ";	
+	size_t contentLengthStartPos=resp.find(contentLengthHeader.c_str());
+	size_t contentLengthEndPos=resp.find("\r\n");
+	string contentLenStr=resp.substr(contentLengthStartPos+contentLengthHeader.length(),contentLengthEndPos);
+	int contentLen=atoi(contentLenStr.c_str());	
+	while(no<=contentLen)
+	{
+		bzero(buff,32000);
+		no+=read(clientSockFileDesc,buff,32000);
+		cout<<buff;
+	}	
+	cout<<"No of bytes read: "<<no<<endl;
+	return no; 
+}
+
+/**
+ * This method is used by readResponse to display the requested file content.
+ * @param
+ *	response: the response containing the header and the file content.
+ */
+void WebClient::displayResponse(string response)
+{
+	size_t fileContentStartPos=response.find("\r\n\r\n");
+	string fileContent=response.substr(fileContentStartPos+4);
+	cout<<fileContent<<endl;
+}
+
+/*
+ * Structure for saving the number of lines in a file and the actual lines of the file
+ */
+struct fileContent
+{
+  int number;
+  string lines[20];
+};
+
+/**
+ * This method is used only in case client requests for persistent connection.
+ * This method is used to read the file in which the list of the files to be requested
+ * is present.
+ * @param
+ *	filename: the name of the file which contains the list of files.
+ * @return
+ *	fileContent structure which contains the number of lines and the actual lines of the file.
+ */
+fileContent getListOfFiles(string filename)
+{
+  
+  fileContent fc;
+  ifstream fileList(filename.c_str());
+  string fileLine;
+  int noOfLines=0;
+  while(!fileList.eof() && getline(fileList,fileLine))
+  { 
+    if(fileLine=="")
+    {
+      break;
+    }
+    fc.lines[noOfLines++]=fileLine;
+  }
+  fc.number=noOfLines;
+  fileList.close();
+  return fc;
+}
+
+/**
+ * This method closes the connection sockets.
+ */
+void WebClient::closeConnection()
+{
+	close(clientSockFileDesc);	
+}
+
+int main(int noOfArguments,char *argumentList[])
+{
+	WebClient client;
+	string connectionType;
+	/*
+         * It checks if all the command-line arguments are provided.
+         */	
+	if(noOfArguments<5)
+	{
+		client.displayError("Invalid arguments!");
+	}
+	client.createSocket();
+	int portNo=atoi(argumentList[2]);
+	client.getServerInfo(argumentList[1]);
+	client.setServerAddress(portNo);
+	client.connectToServer();
+	string filename=argumentList[4];
+	
+	/*
+   	 * Checks if the client has requested for persistent or non-persistent connection.
+    	 */	
+	if(strcmp(argumentList[3],"p")==0)
+	{
+		connectionType="keep-alive";
+	  	fileContent fileList=getListOfFiles(filename);
+	  	string fileLine;
+	  	struct timeval t1, t2;
+	  	double elapsedTime;
+	  	gettimeofday(&t1, NULL);
+	  	for(int i=0;i<fileList.number;i++)
+	  	{
+			
+	    		if(i==(fileList.number-1))
+	    		{
+	      			connectionType="close";
+	    		}
+	    		string request=client.createRequest(fileList.lines[i],connectionType);
+	    		int noOfBytesSent=client.sendRequest(request);
+	    		int noOfBytesRecvd=client.readResponse();
+			cout<<"No of bytes received for file "<<fileList.number<< " : "<<noOfBytesRecvd<<endl;	
+	  	}
+	  	gettimeofday(&t2, NULL);	  	
+	  	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      
+	  	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   
+	  	cout << "RTT: "<<elapsedTime << " ms\n";
+	}
+	else
+	{
+	  struct timeval t1, t2;
+	  double elapsedTime;
+	  gettimeofday(&t1, NULL);
+	  connectionType="close";
+	  string request=client.createRequest(argumentList[4],connectionType);
+	  int noOfCharacters=client.sendRequest(request);
+	  noOfCharacters=client.readResponse();
+	  gettimeofday(&t2, NULL);
+	  elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      
+	  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   
+	  cout << "RTT: "<<elapsedTime << " ms\n";
+	}
+	client.closeConnection();
+	return 0;
+}
---
+++ WebServer.cpp
@@ -0,0 +1,458 @@
+#include<iostream>
+#include<cstdlib>
+#include <unistd.h>
+#include<string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include<fstream>
+#include<map>
+#include <pthread.h>
+#include<sstream>
+#include <sys/stat.h>
+using namespace std;
+
+/**
+ * The WebServer class is a multi-threaded server that is used to service 
+ * multiple client requests. The client can request for either persistent
+ * or non-persistent connections. 
+ * @author Shruti Rachh
+ *
+ */
+class WebServer
+{
+	private:
+	int serverSockFileDesc,clientSockFileDesc;
+	struct sockaddr_in serverAddress,clientAddress;	
+	map<string,string> httpStatusCodes;
+	char *msgContent;
+	int fileSize;
+	
+	public:
+	char buff[1024];
+	void displayError(const char *errorMsg);
+	void setHttpStatusCodes();
+	void createSocket();
+	void bindAddress(int portNumber);
+	void listenConn();	
+	int acceptConn();
+	int readClientRequest(int clientSock);
+	string* processRequest();
+	int getFileSize(const char *filename);
+	string getRequestedContent(string filename);	
+	string setStatusLine(string statusCode,string httpVersion);	
+	string* setResponseHeaders(string fileExt);	
+	string createResponse(string statusCode,string httpVersion,string fileExt);
+	int sendResponse(string response,int clientSock);
+	void closeClientConnection();	
+	void closeConnection();
+};
+
+/**
+ * This method is used to display the error message.
+ * @param
+ * 	errorMsg: The error message that is to be displayed.
+*/
+void WebServer::displayError(const char *errorMsg)
+{
+	cerr<<"Error: "<<errorMsg<<endl;
+	exit(1);
+}
+
+/**
+ * This method is used to set the various HTTP status codes and 
+ * their corresponding HTTP messages.
+ */
+void WebServer::setHttpStatusCodes()
+{
+	httpStatusCodes["200"]="OK";
+	httpStatusCodes["404"]="Page Not Found";
+	httpStatusCodes["400"]="Bad Request";	
+}
+
+/**
+ * This method is used to create the server socket and sets the 
+ * serverSockFileDesc class variable.
+ */
+void WebServer::createSocket()
+{
+	serverSockFileDesc=socket(AF_INET,SOCK_STREAM,0);
+	if (serverSockFileDesc < 0)
+  	{
+		displayError("The server socket could not be opened!");
+	}
+}
+
+/**
+ * This method is used to bind the server socket to an address.
+ * @param
+ * 	portNumber: The port number on which the server will listen for incoming connections.
+ */
+void WebServer::bindAddress(int portNumber)
+{
+	bzero((char *) &serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(portNumber);     	
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+     	
+	if (bind(serverSockFileDesc, (struct sockaddr *) &serverAddress,sizeof(serverAddress)) < 0) 
+  	{      
+		displayError("There is some problem while binding the server socket to an address!");
+	}
+}
+
+/**
+ * In this method, server listens for incoming client connections and defines the queue size 
+ * for the number of client connections that can wait while server is handling 
+ * other connections. The maximum size is 5.
+ */
+void WebServer::listenConn()
+{
+	listen(serverSockFileDesc,5);
+}
+
+/**
+ * This method is used to accept the client connection.
+ * @return
+ *	client socket file descriptor
+ */
+int WebServer::acceptConn()
+{
+	socklen_t clientAddrLength=sizeof(clientAddress);
+  	clientSockFileDesc=accept(serverSockFileDesc,(struct sockaddr *) &clientAddress,&clientAddrLength);
+	return clientSockFileDesc;
+}
+
+/**
+ * This method reads the client request into a buffer using the client socket.
+ * @param
+ *	clientSock: This is the client socket file descriptor.
+ * @return
+ *	the number of characters read
+ */
+int WebServer::readClientRequest(int clientSock)
+{
+	bzero(buff,1024);
+  	int noOfCharacters= read(clientSock,buff,1023);
+  	if (noOfCharacters < 0) 
+	{
+		displayError("There is problem while reading from socket!");
+	}
+	return noOfCharacters;	
+}
+
+/**
+ * This method is used to process the client request.
+ * @return
+ *	a string array consisting of the file name, HTTP version and connection type. 
+ */
+string* WebServer::processRequest()
+{
+	char *filename;
+	string httpVersion;
+	string request=string(buff);
+	string httpVersionStr;
+	string statusCode;
+	size_t connectionTypeStartPos=request.find("Connection: ");
+	size_t connectionTypeEndPos=request.find("\r\n");
+	string connectionType=request.substr(connectionTypeStartPos,connectionTypeEndPos);
+	char *req=strtok(buff,"/");
+	if(req)
+	{
+		
+		req=strtok(NULL," ");
+		if(req)
+		{
+			filename=req;
+			req=strtok(NULL,"\n");
+			if(req)
+			{
+				httpVersionStr=string(req);
+				httpVersion=httpVersionStr.substr(4,(httpVersionStr.size()-5));	
+			}
+		}	
+	}
+	if(strcmp(httpVersionStr.c_str(),"HTTP/1.1")==0 || strcmp(request.substr(0,3).c_str(),"GET")==0)
+	{
+		statusCode="200";
+	}else
+	{
+		statusCode="400";
+	}
+	string* values=new string[4];
+	values[0]=filename;
+	values[1]=httpVersion;
+	values[2]=connectionType;
+	values[3]=statusCode;
+	return values;
+}
+
+/**
+ * Gets the size of the requested file.
+ * @param filename
+ * 	the filename whose size is to be calculated.
+ * @return the file size
+ */
+int WebServer::getFileSize(const char *filename)
+{
+    ifstream file;
+    file.open(filename, ios_base::binary);
+    file.seekg(0,ios_base::end);
+    int size = file.tellg();
+    file.close();
+    return size;
+}
+
+/**
+ * This method gets the requested file contents.
+ * It sets the status code to either 200 or 404 depending 
+ * on whether the file is present or not on the server's 
+ * directory.
+ * @param
+ * 	filename: The name of the file requested.
+ * @return 
+ * 	the HTTP status code
+ */
+string WebServer::getRequestedContent(string filename)
+{
+	int iteration=0;
+	string statusCode;
+	ifstream readFile;
+	readFile.open(filename.c_str());
+	if(readFile.is_open())
+	{
+		fileSize=getFileSize(filename.c_str());
+		msgContent=new char[fileSize];
+		bzero(msgContent,fileSize);
+		int iteration=0;
+		while(!readFile.eof())
+		{
+			readFile.get(msgContent[iteration++]);
+		}
+		readFile.close();
+		statusCode="200";
+	}
+	else
+	{
+		statusCode="404";
+	}
+	
+	return statusCode;
+}
+
+/**
+ * This method is used by the createResponse method in order to set the
+ * status line of the response.
+ * @param
+ *	statusCode: The HTTP status code of the response.
+ * @param
+ * 	httpVersion: The HTTP version of the response.
+ * @return
+ *	the status line string
+ */
+string WebServer::setStatusLine(string statusCode,string httpVersion)
+{
+	string statusLine="HTTP"+httpVersion+" "+statusCode+" "+httpStatusCodes[statusCode];
+	return statusLine;
+}
+
+/**
+ * This method is used by the createResponse method in order to set the
+ * headers of the response.
+ * @param
+ *	fileExt: The file extension used to determine whether the content is of text/html or not.
+ * @return
+ * 	the response headers
+ */
+string* WebServer::setResponseHeaders(string fileExt)
+{
+	string* headers=new string[2];
+	int len=0;
+	if(msgContent!=NULL)
+	{
+		len=strlen(msgContent);
+	}
+	stringstream contentLen;
+	contentLen<<len;
+	headers[0]="Content-Length: "+contentLen.str();
+	if(fileExt==".html")
+	{
+		headers[1]="Content-Type: text/html";
+	}
+	else
+	{
+		headers[1]="Content-Type: text/plain";
+	}	
+	return headers;
+}
+
+/**
+ * This method is used to create the complete HTTP response.
+ * @param
+ *	statusCode: The HTTP status code of the response.
+ * @param
+ * 	httpVersion: The HTTP version of the response.
+ * @param
+ *	fileExt: The file extension used to determine whether the content is of text/html or not.
+ * @return
+ *	the HTTP response
+ */
+string WebServer::createResponse(string statusCode,string httpVersion,string fileExt)
+{		
+	string response;	
+	string statusLine=setStatusLine(statusCode,httpVersion);
+	string* headers=setResponseHeaders(fileExt);
+	if(statusCode=="200")
+	{
+		response=statusLine+"\r\n"+headers[0]+"\r\n"+headers[1]+"\r\n\r\n";
+		for(int i=0;i<fileSize;i++)
+		{
+			response+=msgContent[i];
+		}
+	}else if(statusCode=="404")
+	{
+		if(fileExt==".html")
+		{		
+			response=statusLine+"\r\n"+headers[1]+"\r\n\r\n<html>404 Page Not Found</html>";
+		}
+		else
+		{
+			response=statusLine+"\r\n"+headers[1]+"\r\n\r\n404 File Not Found";
+			cout<<"Response: "<<response<<endl;
+		}
+	}else
+	{
+		response=statusLine+"\r\n"+headers[1]+"\r\n\r\n400 Bad Request";
+	}				
+	return response;		
+}
+
+/**
+ * This method is used to send the response to the client.
+ * @param
+ *	response: the response to be sent
+ * @param
+ * 	clientSock: the client socket
+ * @return
+ *	the number of characters written
+ */
+int WebServer::sendResponse(string response,int clientSock)
+{
+	int noOfCharacters = write(clientSock,(const void *)response.c_str(),response.length());     	
+	if (noOfCharacters < 0)
+	{ 
+		displayError("There is problem while writing to socket!");
+	}
+	cout<<"Number of bytes sent: "<<noOfCharacters<<endl;
+	return noOfCharacters;
+}
+
+/**
+ * This method closes the socket that accepts client connections after the client is been served.
+ */
+void WebServer::closeClientConnection()
+{
+	close(clientSockFileDesc);
+
+}
+
+/**
+ * This method closes the server socket.
+ */
+void WebServer::closeConnection()
+{
+	close(serverSockFileDesc);
+
+}
+
+/**
+ * This is a thread routine which will be called each time a new thread is created.
+ * It is used to read each client requests and respond back to each client.
+ * @param
+ *	clientSocketDesc: the unique client socket which separates each client is used for read and write. 
+ */
+void* handleClientConn(void *clientSocketDesc)
+{
+	WebServer server;
+	int sock = *(int*)clientSocketDesc;
+  	bool flag=true;  	
+	int no=server.readClientRequest(sock);
+    	string* values=server.processRequest();
+	server.setHttpStatusCodes();	
+	while(values[2].find("keep-alive")!=string::npos)
+  	{
+    		   
+    		size_t dotPos=values[0].find(".");
+    		string fileExt=values[0].substr(dotPos);
+		
+		string statusCode;	
+		if(strcmp(values[3].c_str(),"400")!=0)
+		{    		
+			statusCode=server.getRequestedContent(values[0]);
+		}else
+		{	
+			statusCode=values[3];
+		}		
+    		string response=server.createResponse(statusCode,values[1],fileExt);
+    		int noOfChar=server.sendResponse(response,sock);
+		no=server.readClientRequest(sock);
+    		values=server.processRequest();
+    		 
+  	}
+	if(values[2].find("close")!=string::npos)
+	{
+		size_t dotPos=values[0].find(".");
+    		string fileExt=values[0].substr(dotPos);
+		string statusCode;
+			
+		if(strcmp(values[3].c_str(),"400")!=0)
+		{ 		
+			statusCode=server.getRequestedContent(values[0]);
+			
+		}else
+		{	
+			statusCode=values[3];
+		}		
+    		string response=server.createResponse(statusCode,values[1],fileExt);
+    		int noOfChar=server.sendResponse(response,sock);
+	}
+  	server.closeClientConnection();
+}
+
+int main(int noOfArguments,char *argumentList[])
+{
+	WebServer server;
+  	pthread_t clientThread;
+	/*
+         * It checks if all the command-line arguments are provided.
+         */	
+	if(noOfArguments<2)
+	{
+		server.displayError("The client must provide a port number!");
+	}
+	server.createSocket();
+	int portNo=atoi(argumentList[1]);	
+	server.bindAddress(portNo);
+	server.listenConn();
+	int clientSocket;
+	/*
+   	 * It accepts client connections if any and creates a separate thread for each client connection.
+   	 */
+  	while(1)
+  	{
+		clientSocket=server.acceptConn();
+	  	int *tmpClientSock=&clientSocket;
+    		if(pthread_create( &clientThread , NULL , handleClientConn , (void*) tmpClientSock )< 0)
+    		{
+      			server.displayError("The thread could not be created!");
+    		}
+    		pthread_detach(clientThread);
+    		server.listenConn();
+  	}
+	if(clientSocket<0)
+	{
+		server.displayError("There is some problem in accepting connection!");
+	}
+	server.closeConnection();
+	return 0;
+}
